e54869c950c49242b05a08ecd8d06f0122e4759b
Raisul Sohan <lettertosohan@gmail.com>
2026-09-17 10:51:37 +0600

    1.1.4

--- packages/adobe-cep/jsx/lazylord.jsx
+++ packages/adobe-cep/jsx/lazylord.jsx
@@ -7,7 +7,7 @@
 
 if (typeof LazyLord === "undefined") { var LazyLord = {}; }
 
-LazyLord.VERSION = "1.1.3";
+LazyLord.VERSION = "1.1.4";
 
 /** Read a UTF-8 text file and return its contents. */
 LazyLord.readFile = function (path) {
